0e1a6cd396c9b4ee295cda9aa37cab0de32fc107
Timur Abrashin <timurabr.work@gmail.com>
2026-04-11 22:39:42 +0300

    fix(auth): remove KoinContext for desktop

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -4,14 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import org.example.project.presentation.navigation.NavGraph
 import org.example.project.presentation.theme.TaxiAppTheme
-import org.koin.compose.KoinContext
 
 @Composable
 @Preview
 fun App() {
-
     TaxiAppTheme {
         NavGraph()
     }
-
 }
